71705de10d7fd3ff841b453f2ef7a03ebbb65d6d
Matt Beyl <matthew.beyl@gmail.com>
2018-09-04 10:14:28 -0500

    nav bar added to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import NavBar from '../NavBar/NavBar';
+
 
 const Header = ({ title }) => (
   <div className="instructions">
     <div>
       <h1 className="lead">{ title }</h1>
+      <NavBar />
     </div>
   </div>
 );
