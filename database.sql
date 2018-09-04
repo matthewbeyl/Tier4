5b8b9cb98f16a1163403a3b6531cbf3678cb470c
Roboronnie <leex4920@gmail.com>
2018-09-04 09:44:53 -0500

    add sql db, admin content outline

--- database.sql
+++ database.sql
@@ -1,5 +1,80 @@
-CREATE TABLE person (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR (80) UNIQUE NOT NULL,
-    password VARCHAR (1000) NOT NULL
+CREATE DATABASE "tier_four";
+
+CREATE TABLE "users" (
+	"id" serial NOT NULL,
+	"first_name" TEXT NOT NULL,
+	"last_name" TEXT NOT NULL,
+	"email" TEXT NOT NULL,
+	"active" BOOLEAN NOT NULL,
+	"github" TEXT NOT NULL,
+	"queued_for_next_challenge" BOOLEAN NOT NULL DEFAULT 'false',
+	"weekly_email_reminders" BOOLEAN NOT NULL DEFAULT 'true',
+	"daily_email_reminders" BOOLEAN NOT NULL DEFAULT 'true',
+	"admin" BOOLEAN NOT NULL DEFAULT 'false',
+	CONSTRAINT users_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
+
+
+
+CREATE TABLE "challenges" (
+	"id" serial NOT NULL,
+	"title" TEXT NOT NULL UNIQUE,
+	"date" DATE NOT NULL UNIQUE,
+	"exclude_weekends" BOOLEAN NOT NULL DEFAULT 'false',
+	"exclude_holidays" BOOLEAN NOT NULL DEFAULT 'false',
+	CONSTRAINT challenges_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "user_challenge" (
+	"id" serial NOT NULL,
+	"user_id" int NOT NULL,
+	"challenge_id" int NOT NULL,
+	"longest_streak" int NOT NULL,
+	"commit_percentage" int NOT NULL,
+	CONSTRAINT user_challenge_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "email_termination_reason" (
+	"id" serial NOT NULL,
+	"user_id" int NOT NULL,
+	"reason" TEXT NOT NULL,
+	CONSTRAINT email_termination_reason_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "weekly_progress_form" (
+	"id" serial NOT NULL,
+	"user_id" int NOT NULL,
+	"applied" TEXT NOT NULL,
+	"learned" TEXT NOT NULL,
+	"built" TEXT NOT NULL,
+	"followed_up" TEXT NOT NULL,
+	"events_networking" TEXT NOT NULL,
+	CONSTRAINT weekly_progress_form_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "user_challenge" ADD CONSTRAINT "user_challenge_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+ALTER TABLE "user_challenge" ADD CONSTRAINT "user_challenge_fk1" FOREIGN KEY ("challenge_id") REFERENCES "challenges"("id");
+
+ALTER TABLE "email_termination_reason" ADD CONSTRAINT "email_termination_reason_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
+ALTER TABLE "weekly_progress_form" ADD CONSTRAINT "weekly_progress_form_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
--- src/components/AdminView/AdminView.js
+++ src/components/AdminView/AdminView.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+    user: state.user,
+    login: state.login,
+    // saga should hold user data, get it here 
+  });
 
 class AdminView extends Component {
+
+    componentDidMount() {
+        // on mounth, dispatch an action to saga - axios - db to get user data 
+    }
+
     render() {
         return (
             <main>
                 <h1>This is the Admin View</h1>
+                {/* display button for admin to make new challenge, 
+                render conditionally*/}
+                <section>
+                    <button>Create New Challenge</button>
+                </section>
+                {/* always viewable, two buttons that changes which table to show */}
+                <section>
+                    <button>Current Challenge</button>
+                    <button>Past Challenges</button>
+                </section>
+                {/* display either  */}
             </main>
         )
     }
 }
 
-export default AdminView;
+export default connect(mapStateToProps)(AdminView);
