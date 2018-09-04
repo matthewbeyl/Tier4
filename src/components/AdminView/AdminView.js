3716e81f186cf69aff3d7b283ba8ca0ff216c462
Roboronnie <leex4920@gmail.com>
2018-09-04 09:33:05 -0500

    Create Home, Dashboard, and Admin View components

---
+++ src/components/AdminView/AdminView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AdminView extends Component {
+    render() {
+        return (
+            <main>
+                <h1>This is the Admin View</h1>
+            </main>
+        )
+    }
+}
+
+export default AdminView;
