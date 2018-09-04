783aa179a645b20e27424f18e73e31e2402e7153
Matt Beyl <matthew.beyl@gmail.com>
2018-09-04 10:53:09 -0500

    add info message to home page

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -7,6 +7,14 @@ class HomeView extends Component {
             <main>
                 <NavBar />
                 <h1>This is the Home View</h1>
+                <p>
+                    Tier Four is the next step in your career as a Full Stack Developer.
+                    Code challenges will be initiated periodically by the Prime Digital Academy staff, 
+                    to encourage you to continue committing code once you are out of the classroom.
+
+                    The goal?
+                    Achieve and maintain the highest daily commit rate that you can, and ear your spot on the leaderboard!
+                </p>
             </main>
         )
     }
