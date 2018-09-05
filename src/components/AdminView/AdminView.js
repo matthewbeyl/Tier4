bf91ce68949e71ceff1d7e3d04edaab6360c28d8
Roboronnie <leex4920@gmail.com>
2018-09-05 10:01:56 -0500

    add function to create new challenge button to display popup form

--- src/components/AdminView/AdminView.js
+++ src/components/AdminView/AdminView.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../Header/Header';
 
+class PopupForm extends React.Component {
+    render() {
+        return (
+            <div style={{ display: 'flex', position: 'fixed', width: '100%', height: '100%', top: '0', left: '0', right: '0', bottom: '0', margin: 'auto', backgroundColor: 'rgba(0,0,0,0.5)' }}>
+                <div style={{ alignContent: 'center', postion: 'absolute', left: '25%', right: '25%', top: '25%', bottom: '25%', margin: 'auto', backgroundColor: 'white' }}>
+                    <p>{this.props.text}</p>
+                    <form>
+                        <label>Challenge Title</label>
+                        <input type="text" placeholder="Title" />
+                        <label>Start Date</label><br />
+                        <input type="date" />
+                        <button>Create Challenge</button>
+                    </form>
+                    <button onClick={this.props.closePopupForm}>Cancel</button>
+                </div>
+            </div>
+        );
+    }
+}
+
 const mapStateToProps = state => ({
     user: state.user,
     login: state.login,
@@ -12,22 +32,40 @@ class AdminView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            current: '',
+            showPopupForm: false
         }
     }
+
     componentDidMount() {
         this.props.dispatch({ type: 'FETCH_CURRENT_CHALLENGE' });
     }
 
+    showCurrentChallenge = () => {
+        console.log('current challenge button clicked');
+    }
+
+    showPastChallenges = () => {
+        console.log('past challenge button clicked');
+    }
+
+    toggleCreateNewChallengePopupForm = () => {
+        this.setState({
+            showPopupForm: !this.state.showPopupForm
+        });
+    }
     render() {
         let apiChallengeResults = null;
-        // map over mapStateToProps state here for user data
         this.props.currentChallengeData.map((user, index) => {
             apiChallengeResults = user.map((eachUser, index) => {
-                return(
-                    <div key={index}>
-                        {eachUser.first_name} {eachUser.last_name}
-                    </div>
+                return (
+                    <tr key={index}>
+                        <td>{eachUser.first_name} {eachUser.last_name}</td>
+                        <td>{eachUser.commit_percentage}</td>
+                        <td>{eachUser.longest_streak}</td>
+                        <td>{eachUser.daily_email_reminders.toString()}</td>
+                        <td>{eachUser.weekly_email_reminders.toString()}</td>
+                        <td><button>Delete</button></td>
+                    </tr>
                 )
             })
         })
@@ -37,18 +75,35 @@ class AdminView extends Component {
             <main>
                 <Header title="Tier Four" />
                 <h1>This is the Admin View</h1>
-                {/* display button for admin to make new challenge, 
-                render conditionally*/}
-                <section>
-                    <button>Create New Challenge</button>
-                </section>
-                {/* always viewable, two buttons that changes which table to show */}
-                <section>
-                    <button>Current Challenge</button>
-                    <button>Past Challenges</button>
-                </section>
 
-                {apiChallengeResults}
+                <button onClick={this.toggleCreateNewChallengePopupForm.bind(this)}>Create New Challenge</button>
+                {this.state.showPopupForm ?
+                    <PopupForm
+                        text='Create a New Challenge'
+                        closePopupForm={this.toggleCreateNewChallengePopupForm.bind(this)}
+                    />
+                    : null
+                }
+                <button onClick={this.showCurrentChallenge}>Current Challenge</button>
+                <button onClick={this.showPastChallenges}>Past Challenges</button>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Commit %</th>
+                            <th>Streak</th>
+                            <th>Daily Reminder</th>
+                            <th>Weekly Reminder</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {apiChallengeResults}
+                    </tbody>
+                </table>
+
+
             </main>
         )
     }
