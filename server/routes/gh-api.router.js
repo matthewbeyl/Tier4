1c52e391821d5b84b5257ccdf9aac2c65baa94e5
Tyler Sehr <tysehr37@gmail.com>
2018-09-06 10:28:57 -0500

    updated query

--- server/routes/gh-api.router.js
+++ server/routes/gh-api.router.js
@@ -5,14 +5,21 @@ const rp = require('request-promise')
 
 
 let userList = []
+var date = new Date();
+date.setMonth(date.getMonth() -1);
+// date = date.toString()
+date = JSON.stringify(date)
+actualDate = date.substring(1, 11)
+console.log(date, date.substring(1, 11));
+
 
 router.get('/get-gh-data', (req, res)=>{
     console.log('getting gh data');
     const requestPromises = []
     userList.forEach(user=>{
         const requestOptions = {
-            uri: `https://api.github.com/users/${user.github}/events`,
-            headers : {"User-Agent": 'ReverendEd'},
+            uri: `https://api.github.com/search/commits?q=committer:${user.github}+committer-date:>${actualDate}&sort=committer-date&per_page=100`,
+            headers : {"User-Agent": user.github, Accept: 'application/vnd.github.cloak-preview+json'},
             method: 'GET',
             json: true
         }
@@ -27,17 +34,63 @@ router.get('/get-gh-data', (req, res)=>{
     })
 })
 
-router.get('/get-user-list', (req, res)=>{
+
+
+// router.get('/get-gh-data', (req, res) => {
+//     console.log('getting gh data');
+//     const requestPromises = []
+//     userList.forEach(user => {
+//         const requestOptions = {
+//             uri: 'https://api.github.com/graphql',
+//             headers: {
+//                 "User-Agent": 'reverended',
+//                 Authorization: 'Bearer 93646e68719f4ca35e728060af4e1e34fe45cf32'
+//             },
+//             method: 'POST',
+//             body: {
+//                 query: `
+//                 query {
+//                     user(login: ${user.github}){
+//                         repositories(first: 1, orderBy: {field:PUSHED_AT, direction: DESC}){
+//                         nodes{
+//                           name
+//                           pushedAt
+//                           description
+//                         }
+//                       }
+//                     }
+                   
+//                    }
+//                 `
+//               },
+//             json: true
+//     }
+//         requestPromises.push(rp(requestOptions));
+// })
+// Promise.all(requestPromises)
+//     .then((data) => {
+//         res.send(data)
+//     })
+//     .catch((error) => {
+//         console.log(error);
+
+//         res.sendStatus(500)
+//     })
+// })
+
+
+
+router.get('/get-user-list', (req, res) => {
     console.log('getting user list');
     const queryText = 'SELECT * FROM "users" WHERE "active" = TRUE';
     pool.query(queryText)
-    .then((response)=>{
-        userList = response.rows
-        res.send(userList)
-    })
-    .catch((error)=>{
-        console.log('error on get-user-list',error);
-    })
+        .then((response) => {
+            userList = response.rows
+            res.send(userList)
+        })
+        .catch((error) => {
+            console.log('error on get-user-list', error);
+        })
 })
 
 /**
--- src/components/TestComponent/TestComponent.js
+++ src/components/TestComponent/TestComponent.js
@@ -10,18 +10,37 @@ class TestComponent extends Component {
     }
 
     componentDidMount(){
-        this.getUserList();
+
+        // axios({
+        //     url: 'https://api.github.com/graphql',
+        //     headers: {
+        //         "User-Agent": 'reverended',
+        //         Authorization: 'Bearer 93646e68719f4ca35e728060af4e1e34fe45cf32'
+        //     },
+        //     method: 'POST',
+        //     data: {
+        //         query: `
+        //             query {user(login: "${github.user}"){
+        //                 repositories(last: 100){
+        //                     nodes{
+        //                         name,
+        //                         pushedAt,
+        //                         description
+        //                     }
+        //                 }
+        //             }  
+        //         }
+        //         `
+        //       }
+        // })
+        // .then((response)=>{
+        //     console.log(response);
+        // })
+        // .catch((error)=>{
+        //     console.log(error);
+        // })
     }
 
-    getUserList = ()=>{
-        axios({
-            url: '/api/gh-router/get-user-list',
-            method: 'GET'
-        })
-        .then((response)=>{
-            console.log(response.data);
-        })
-    }
 
     getGHData = ()=>{
         axios({
