2787e019a7482eca97e17506168b719195f5d0ff
Tyler Sehr <tysehr37@gmail.com>
2018-09-06 10:30:44 -0500

    fixed test component

--- src/components/TestComponent/TestComponent.js
+++ src/components/TestComponent/TestComponent.js
@@ -10,35 +10,35 @@ class TestComponent extends Component {
     }
 
     componentDidMount(){
-        // this.getUserList();
-        // axios({
-        //     url: 'https://api.github.com/graphql',
-        //     headers: {
-        //         "User-Agent": 'reverended',
-        //         Authorization: 'Bearer 93646e68719f4ca35e728060af4e1e34fe45cf32'
-        //     },
-        //     method: 'POST',
-        //     data: {
-        //         query: `
-        //             query {user(login: "${github.user}"){
-        //                 repositories(last: 100){
-        //                     nodes{
-        //                         name,
-        //                         pushedAt,
-        //                         description
-        //                     }
-        //                 }
-        //             }  
-        //         }
-        //         `
-        //       }
-        // })
-        // .then((response)=>{
-        //     console.log(response);
-        // })
-        // .catch((error)=>{
-        //     console.log(error);
-        // })
+        this.getUserList();
+    //     axios({
+    //         url: 'https://api.github.com/graphql',
+    //         headers: {
+    //             "User-Agent": 'reverended',
+    //             Authorization: 'Bearer 93646e68719f4ca35e728060af4e1e34fe45cf32'
+    //         },
+    //         method: 'POST',
+    //         data: {
+    //             query: `
+    //                 query {user(login: "${github.user}"){
+    //                     repositories(last: 100){
+    //                         nodes{
+    //                             name,
+    //                             pushedAt,
+    //                             description
+    //                         }
+    //                     }
+    //                 }  
+    //             }
+    //             `
+    //           }
+    //     })
+    //     .then((response)=>{
+    //         console.log(response);
+    //     })
+    //     .catch((error)=>{
+    //         console.log(error);
+    //     })
     }
 
     getUserList = ()=>{
