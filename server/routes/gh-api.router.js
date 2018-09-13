d78bb08cf935d0f088a1fa85ac2163803ffba108
Tyler Sehr <tysehr37@gmail.com>
2018-09-13 11:22:32 -0500

    fixed rate limit bug

--- server/routes/gh-api.router.js
+++ server/routes/gh-api.router.js
@@ -2,11 +2,12 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const rp = require('request-promise')
+const request = require('request')
 const cron = require('node-cron');
 
 
 
-// let userList = []
+let listOfUsers = []
 let challengeDate = '01-01-2018'
 let challengeID = 0;
 let challengeDateString = '01-01-2018'
@@ -25,12 +26,12 @@ date = JSON.stringify(date)
 todaysDate = date.substring(1, 11)
 //calculate todays date so that we only get todays commits in getData()
 
-cron.schedule('*/20 * * * * *', function () {
-    console.log('running once every 20 seconds');
+cron.schedule('*/600 * * * * *', function () {
+    console.log('running once every 10 min');
     getData();
 });
 //run getData() once every 20 seconds, will be changed to once a day at midnight.
-
+//getData();
 
 router.get('/get-data', (req, res) => {
     res.send(theData)
@@ -60,61 +61,14 @@ function getData() {
                         })
                         .catch((error) => {
                             console.log('could not deactivate challenge', error);
-
                         })
                 }
                 pool.query(`SELECT "github", "user_id", "calendar" FROM "users"
                 JOIN "user_challenge" ON "users"."id" = "user_challenge"."user_id"
                 WHERE "user_challenge"."challenge_id" = $1;`, [challengeID]) //retrieves the github, user_id, and calendar for users enrolled in the current challenge, using the challenge id just retrieved.
                     .then((response) => {
-                        let tempUserList = response.rows
-                        //sets the userlist to the data we just got back.
-                        //reg for loop for batching
-                        let batchedUserList = [];
-                        let thisBatch = [];
-                        let counter = 0;
-                        for (let i = 0; i < tempUserList.length; i++) {
-                            if (counter < 15) {
-                                thisBatch.push(tempUserList[i])
-                                counter++;
-                                if (i === tempUserList.length) {
-                                    counter = 0;
-                                    batchedUserList.push(thisBatch);
-                                    thisBatch = [];
-                                }
-                            }
-                            else if (counter >= 15) {
-                                counter = 0;
-                                batchedUserList.push(thisBatch)
-                                thisBatch = [];
-                            }
-                        }
-
-                        batchedUserList.forEach(batch => {
-                            console.log('getting gh data');
-                            const requestPromises = [] //create an array to store all the requests we will send to the github api
-                            batch.forEach(user => { //loop through the userlist we just got from postgres and generate an api call using each users information.
-                                const requestOptions = {
-                                    uri: `https://api.github.com/search/commits?q=committer:${user.github}+committer-date:${todaysDate}&sort=committer-date&per_page=1`,
-                                    headers: { "User-Agent": user.github, Accept: 'application/vnd.github.cloak-preview+json', Authorization: 'token 23982af669baa75e29e52bbd5a45594c65b7f7b2' },
-                                    method: 'GET',
-                                    json: true
-                                }
-                                requestPromises.push(rp(requestOptions)); //create a promise for each api request
-                            })
-                            Promise.all(requestPromises) //run and wait for all promises to be fullfilled.
-                                .then((data) => {
-                                    theData = data; //set the response data to theData variable so that testcomponent can access it through the endpoint '/get-data'
-                                    sortAndSendData(data, batch); //parses through the data before posting the processed data to postgres
-                                })
-                                .catch((error) => {
-                                    console.log(error);
-                                })
-
-                            setTimeout(() => {
-                                console.log('waiting for github api to refresh');
-                            }, 60000);
-                        })
+                        listOfUsers = response.rows
+                        callApi(listOfUsers.shift())
                     })
                     .catch((error) => {
                         console.log('error on get-user-list', error);
@@ -126,8 +80,28 @@ function getData() {
         })
 }
 
+function callApi(user) {
+    const requestPromises = [] //create an array to store all the requests we will send to the github api
+     //loop through the userlist we just got from postgres and generate an api call using each users information.
+        const requestOptions = {
+            uri: `https://api.github.com/search/commits?q=committer:${user.github}+committer-date:${todaysDate}&sort=committer-date&per_page=1`,
+            headers: { "User-Agent": user.github, Accept: 'application/vnd.github.cloak-preview+json', Authorization: 'token 23982af669baa75e29e52bbd5a45594c65b7f7b2' },
+            method: 'GET',
+            json: true
+        }
+        requestPromises.push(rp(requestOptions)); //create a promise for each api request
+    Promise.all(requestPromises)
+    .then((data) => {
+        console.log(user.github, data);
+        sortAndSendData(data, user)
+        if (listOfUsers.length !== 0) {
+            setTimeout(()=>callApi(listOfUsers.shift()), 2000)
+        }
+    })
+}
 
-async function sortAndSendData(tempData, userList) { //tempData is an array of the data(sorted by user) that we got from the api. its indexes correspond to the userList array. this is important.
+async function sortAndSendData(tempData, user) { //tempData is an array of the data(sorted by user) that we got from the api. its indexes correspond to the userList array. this is important.
+    console.log('sorting and sending this users data');
     let date = JSON.stringify(challengeDate)
     let currentDate = JSON.stringify(new Date())
     let date1 = new Date(date.substring(1, 11));
@@ -135,34 +109,36 @@ async function sortAndSendData(tempData, userList) { //tempData is an array of t
     let timeDiff = Math.abs(date2.getTime() - date1.getTime());
     let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); //format and compare todays date to the challenge start date. grabs diffDays to get commit %
 
-    for (let i = 0; i < tempData.length; i++) { //loop through tempdata and store a specific users data into these temporary variables.
-        let tempUserData = tempData[i].items; //list of the commits a user made, should only have 1 item.
-        let tempUserName = userList[i]; //grabs user info of the corresponding user.
-        let tempDate = challengeDate; //i dont think this is ever used, will remove one day......
+    //for (let i = 0; i < tempData.length; i++) { //loop through tempdata and store a specific users data into these temporary variables.
+    let tempUserData = tempData[0].total_count; //list of the commits a user made, should only have 1 item.
+    let tempUserName = user; //grabs user info of the corresponding user.
+    let tempDate = challengeDate; //i dont think this is ever used, will remove one day......
 
-        let calendar = await processData(tempUserData, diffDays, tempUserName) //updates and bundles together the users calendar
+    let calendar = await processData(tempUserData, diffDays, tempUserName) //updates and bundles together the users calendar
 
-        let processedData = getStreakAndPercent(calendar, diffDays) //calculate the streak and commit % using the calendar we just updated
+    let processedData = getStreakAndPercent(calendar, diffDays) //calculate the streak and commit % using the calendar we just updated
 
-        let data = packageData(tempUserName, processedData); //package all the data we just generated into a single object for simplicity
+    let data = packageData(tempUserName, processedData); //package all the data we just generated into a single object for simplicity
 
-        setActiveUser(calendar, tempUserName, diffDays)
-        //this is where everything has finished ok
+    setActiveUser(calendar, tempUserName, diffDays)
+    //this is where everything has finished ok
 
-        pool.query(`UPDATE "user_challenge" SET "longest_streak" = $1, "commit_percentage" = $2, "calendar" = $3 WHERE "user_id" = $4`,
-            [data.longestStreak, data.commitPercent, calendar, data.userID])
-            .then((response) => { //sends the data we just generated to the user_challenge table in postgres. will update existing entries for users. user entries are generated upon joining a challenge.
-                //console.log(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
+    pool.query(`UPDATE "user_challenge" SET "longest_streak" = $1, "commit_percentage" = $2, "calendar" = $3 WHERE "user_id" = $4`,
+        [data.longestStreak, data.commitPercent, calendar, data.userID])
+        .then((response) => { //sends the data we just generated to the user_challenge table in postgres. will update existing entries for users. user entries are generated upon joining a challenge.
+            //console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    //}
 }
 
 function setActiveUser(calendar, userInfo, diffDays) {
-    tempCalendar = calendar.slice(0, diffDays)
-    tempCalendar.reverse();
+    tempCalendar = calendar.slice(0, diffDays+1)
+    tempCalendar = tempCalendar.reverse();
+    console.log(tempCalendar, userInfo.github);
+
     if (tempCalendar[0] === false && tempCalendar[1] === false && tempCalendar[2] === false && tempCalendar[3] === false && tempCalendar[4] === false) {
         console.log(`setting ${userInfo.github} to inactive.`);
 
@@ -185,7 +161,7 @@ function setActiveUser(calendar, userInfo, diffDays) {
 
 function processData(userData, diffDays, username) {
     let userCommitArray = username.calendar
-    if (userData.length !== 0 && diffDays <= 30) { //set the corresponding array index to the value true if the user has committed that day.
+    if (userData >= 1 && diffDays <= 30) { //set the corresponding array index to the value true if the user has committed that day.
         userCommitArray[diffDays] = true;         //if the user committed on the second day of the challenge, the second value in userCommitArray will be true, the rest will be false
     }
     else if (diffDays > 30) {
@@ -275,140 +251,6 @@ function activateChallenge() {
         })
 }
 
-
-
-
-
-// function sortData(tempData){
-//     let date = JSON.stringify(challengeDate)
-//     let currentDate = JSON.stringify(new Date())
-//     let date1 = new Date(date.substring(1, 11));
-//     let date2 = new Date(currentDate.substring(1, 11));
-//     let timeDiff = Math.abs(date2.getTime() - date1.getTime());
-//     let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-
-//     for (let i = 0; i < tempData.length; i++) {
-//         let tempUserData = tempData[i].items;
-//         let tempUserName = userList[i];
-//         let tempDate = challengeDate;
-//         let processedData = getStreakAndPercent(processData(tempUserData, tempDate), diffDays)
-
-//         let data = packageData(tempUserName, processedData);
-//         console.log(data);
-
-//         //this is where everything has finished ok
-//     }
-// }
-
-// function processData(userData, datestring){
-//     let userCommitArray = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
-//     for (let i = 0; i < userCommitArray.length; i++) {
-//         let date = new Date(datestring)
-//         date.setDate(date.getDay() + i);       
-//         userData.forEach(commit=>{
-//             let tempDate = JSON.stringify(date)
-//             let commitDate = JSON.stringify(commit.commit.author.date)
-//             if (commitDate.substring(1, 11) == tempDate.substring(1, 11)) {
-//                 userCommitArray[i]= true;
-//             }
-//         })
-//     }
-//     return userCommitArray;
-// }
-
-// function packageData(username, data){
-//     data.userID = username.id;
-//     data.challengeID = challengeDateID;
-//     return data
-// }
-
-// function getStreakAndPercent(data, diffDays){
-//     let longestStreak = getStreak(data)
-//     let commitPercent = getPercent(data, diffDays)
-//     //console.log(longestStreak, commitPercent);
-//     return {
-//         longestStreak,
-//         commitPercent
-//     }
-// }
-
-// function getStreak(data){
-//     let currentStreak = 0;
-//     let maxStreak = 0;
-//     let previousVal = false;
-//     data.forEach(index=>{
-//         if (index === true) {
-//             previousVal = true;
-//         }
-//         if (index && previousVal) {
-//             currentStreak++;
-//         }
-//         if (currentStreak > maxStreak) {
-//             maxStreak = currentStreak;
-//         }
-//         if (index === false) {
-//             previousVal = false;
-//             currentStreak = 0;
-//         }
-//     })
-//     return maxStreak
-// }
-
-// function getPercent(data, diffDays){
-//     let newData = data.slice(0, diffDays)
-//     //console.log('dataArray',newData );
-//     let commitCount = 0;
-//     for (let i = 0; i < newData.length; i++) {
-//         if (newData[i]) {
-//             commitCount++
-//         }
-//     }
-//     commitCount = commitCount/diffDays * 100;
-
-//     return commitCount
-// }
-
-// // router.get('/get-gh-data', (req, res) => {
-// //     console.log('getting gh data');
-// //     const requestPromises = []
-// //     userList.forEach(user => {
-// //         const requestOptions = {
-// //             uri: 'https://api.github.com/graphql',
-// //             headers: {
-// //                 "User-Agent": 'reverended',
-// //                 Authorization: 'Bearer 93646e68719f4ca35e728060af4e1e34fe45cf32'
-// //             },
-// //             method: 'POST',
-// //             body: {
-// //                 query: `
-// //                 query {
-// //                     user(login: ${user.github}){
-// //                         repositories(first: 1, orderBy: {field:PUSHED_AT, direction: DESC}){
-// //                         nodes{
-// //                           name
-// //                           pushedAt
-// //                           description
-// //                         }
-// //                       }
-// //                     }
-
-// //                    }
-// //                 `
-// //               },
-// //             json: true
-// //     }
-// //         requestPromises.push(rp(requestOptions));
-// // })
-// // Promise.all(requestPromises)
-// //     .then((data) => {
-// //         res.send(data)
-// //     })
-// //     .catch((error) => {
-// //         console.log(error);
-
-// //         res.sendStatus(500)
-// //     })
-// // })
 
 
 
